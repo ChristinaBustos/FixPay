66843053a69e6482d48c042c25a161f8fa62cdd6
20213tn048 <angel_camargo2020@outlook.com>
2023-10-07 04:26:40 -0600

    IA integration

---
+++ backend/src/utils/q&a.js
@@ -0,0 +1,48 @@
+const questions = [
+    'Cuentas Bancarias',
+    'Pagos',
+    'Tarjetas',
+    'Sobre Nosotros'
+]
+
+const first = [
+    '¿Qué es una cuenta de ahorros?',
+    '¿Por qué es importante tener una cuenta bancaria?',
+    '¿En qué se diferencia una cuenta corriente de una cuenta de ahorros?'
+]
+
+const second = [
+    '¿Qué tipos de pagos existen?',
+    '¿Que significa realizar un pago?',
+    '¿Por qué es beneficioso usar aplicaciones bancarias móviles?'
+]
+
+const third = [
+    '¿Qué es una tarjeta de débito y cómo funciona?',
+    '¿Cuál es la diferencia entre una tarjeta de débito y una de crédito?',
+    '¿Qué es el CVV de una tarjeta y por qué es importante?'
+]
+
+const fourth = [
+    '¿Qué es Bonanza?',
+    '¿Cuál es el objetivo principal de Bonanza?',
+    '¿Qué es un score crediticio?'
+]
+
+
+const res = [
+    'Bonanza es una aplicación móvil que se enfoca en educar a los usuarios sobre la gestión financiera y cómo mejorar su score crediticio.',
+    'El objetivo principal de Bonanza es empoderar a los usuarios para tomar decisiones financieras informadas',
+    'Un score crediticio es un número que representa la probabilidad de que un individuo pague sus deudas a tiempo. Este número se calcula utilizando la información de su informe de crédito. El puntaje de crédito es un número entre 300 y 850, siendo 300 el peor y 850 el mejor. Cuanto más alto sea su puntaje, más probable será que los prestamistas le otorguen un préstamo.',
+]
+
+
+
+module.exports = {
+    questions,
+    first,
+    second,
+    third,
+    fourth,
+    res
+}
